af71b6179db2b8ebcd83be9676e5343ed06af0de
yoichiro <yoichiro@eisbahn.jp>
2011-06-29 09:26:52 +0900

    Refactoring MixiContainerWrapperImpl.

---
+++ src/jp/mixi/android/sdk/wrapper/AbstractContainerImpl.java
@@ -0,0 +1,32 @@
+package jp.mixi.android.sdk.wrapper;
+
+import jp.mixi.android.sdk.MixiContainer;
+
+/**
+ * 各種コンテナクラスに共通な処理を提供する抽象クラスです.
+ * @author Yoichiro Tanaka
+ *
+ */
+public abstract class AbstractContainerImpl {
+
+    /** MixiContainerオブジェクト. */
+    private MixiContainer container;
+
+    /**
+     * 指定されたパラメータでこのインスタンスを初期化します.
+     * @param newContainer MixiContainerオブジェクト
+     */
+    public AbstractContainerImpl(final MixiContainer newContainer) {
+        super();
+        this.container = newContainer;
+    }
+
+    /**
+     * MixiContainerオブジェクトを返します.
+     * @return MixiContainerオブジェクト
+     */
+    protected MixiContainer getContainer() {
+        return container;
+    }
+
+}
