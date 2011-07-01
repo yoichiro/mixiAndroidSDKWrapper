9e80aa9c052b6f24c9a8952d92f0ad465e28c39b
yoichiro <yoichiro@eisbahn.jp>
2011-07-01 09:29:16 +0900

    Adding the handler class to parse comments.

---
+++ src/jp/eisbahn/android/sdk/wrapper/photo/GetCommentsCallbackHandler.java
@@ -0,0 +1,103 @@
+package jp.eisbahn.android.sdk.wrapper.photo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jp.eisbahn.android.sdk.wrapper.CallbackAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+
+/**
+ * コメント情報の取得結果を受け取り提供するためのコールバックハンドラクラスです.
+ * @author Yoichiro Tanaka
+ *
+ */
+public class GetCommentsCallbackHandler extends CallbackAdapter {
+
+    /** 総取得可能件数. */
+    private int totalResults;
+
+    /** 今回要求分に関する取得件数. */
+    private int itemsPerPage;
+
+    /** 開始インデックス. */
+    private int startIndex;
+
+    /** コメント情報のコレクション. */
+    private List<Comment> comments;
+
+    /**
+     * 指定された情報でこのインスタンスを初期化します.
+     * @param newContext コンテキストオブジェクト
+     */
+    public GetCommentsCallbackHandler(final Context newContext) {
+        super(newContext);
+    }
+
+    @Override
+    public void onComplete(final Bundle values) {
+        try {
+            String response = values.getString("response");
+            JSONObject root = new JSONObject(response);
+            totalResults = root.getInt("totalResults");
+            itemsPerPage = root.getInt("itemsPerPage");
+            startIndex = root.getInt("startIndex");
+            Object entry = root.get("entry");
+            if (entry instanceof JSONObject) {
+                JSONObject obj = (JSONObject) entry;
+                comments = new ArrayList<Comment>();
+                comments.add(new Comment(obj));
+            } else {
+                JSONArray array = (JSONArray) entry;
+                comments = new ArrayList<Comment>();
+                for (int i = 0; i < array.length(); i++) {
+                    JSONObject obj = array.getJSONObject(i);
+                    comments.add(new Comment(obj));
+                }
+            }
+            super.onComplete(values);
+        } catch (JSONException e) {
+            Log.e("mixiAndroidSDKWrapper", e.getMessage(), e);
+            alert("Error", "Fetching albums failed. " + e.getMessage());
+        }
+    }
+
+    /**
+     * 総取得可能件数を返します.
+     * @return 総取得可能件数
+     */
+    public int getTotalResults() {
+        return totalResults;
+    }
+
+    /**
+     * 今回要求分に関する取得件数を返します.
+     * @return 今回要求分に関する取得件数
+     */
+    public int getItemsPerPage() {
+        return itemsPerPage;
+    }
+
+    /**
+     * 開始インデックスを返します.
+     * @return 開始インデックス
+     */
+    public int getStartIndex() {
+        return startIndex;
+    }
+
+    /**
+     * コメント情報のコレクションを返します.
+     * @return コメント情報のコレクション
+     */
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+}
