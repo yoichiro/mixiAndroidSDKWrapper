31323b2581d400de06b632ded60137a75630e708
yoichiro <yoichiro@eisbahn.jp>
2011-08-20 11:29:00 +0900

    Supporting a diary post with an image.

--- src/jp/eisbahn/android/sdk/wrapper/DiaryAPI.java
+++ src/jp/eisbahn/android/sdk/wrapper/DiaryAPI.java
@@ -1,5 +1,7 @@
 package jp.eisbahn.android.sdk.wrapper;
 
+import java.io.InputStream;
+
 /**
  * Diary API を規定するインタフェースです.
  * @author Yoichiro Tanaka
@@ -61,5 +63,65 @@ public interface DiaryAPI {
      */
     void postDiary(String title, String body, String group,
             boolean showUsers, GetIdCallbackHandler handler);
+
+    /**
+     * 指定された情報で全体公開される日記を投稿します.
+     * show_usersパラメータ値は「自分と公開中の友人に表示」が適用されます。
+     * @param title 日記のタイトル文字列
+     * @param body 日記の本文
+     * @param image 画像のストリーム
+     * @param handler 処理結果を扱うためのハンドラオブジェクト
+     */
+    void postPublicDiary(String title, String body, InputStream image,
+            GetIdCallbackHandler handler);
+
+    /**
+     * 指定された情報で日記を投稿します.
+     * show_usersパラメータ値は「自分だけに表示」が適用されます。
+     * @param title 日記のタイトル文字列
+     * @param body 日記の本文
+     * @param visibility 日記の公開設定
+     * @param image 画像のストリーム
+     * @param handler 処理結果を扱うためのハンドラオブジェクト
+     */
+    void postDiary(String title, String body, Visibility visibility,
+            InputStream image, GetIdCallbackHandler handler);
+
+    /**
+     * 指定された情報で日記を投稿します.
+     * show_usersパラメータ値は「自分だけに表示」が適用されます。
+     * @param title 日記のタイトル文字列
+     * @param body 日記の本文
+     * @param group 公開対象のグループID
+     * @param image 画像のストリーム
+     * @param handler 処理結果を扱うためのハンドラオブジェクト
+     */
+    void postDiary(String title, String body, String group,
+            InputStream image, GetIdCallbackHandler handler);
+
+    /**
+     * 指定された情報で日記を投稿します.
+     * @param title 日記のタイトル文字列
+     * @param body 日記の本文
+     * @param visibility 日記の公開設定
+     * @param showUsers 「自分だけに表示」の場合はfalse,「自分と公開中の友人に表示」の場合はtrue
+     * @param image 画像のストリーム
+     * @param handler 処理結果を扱うためのハンドラオブジェクト
+     */
+    void postDiary(String title, String body, Visibility visibility,
+            boolean showUsers, InputStream image, GetIdCallbackHandler handler);
+
+    /**
+     * 指定された情報で日記を投稿します.
+     * show_usersパラメータ値は「自分だけに表示」が適用されます。
+     * @param title 日記のタイトル文字列
+     * @param body 日記の本文
+     * @param group 公開対象のグループID
+     * @param showUsers 「自分だけに表示」の場合はfalse,「自分と公開中の友人に表示」の場合はtrue
+     * @param image 画像のストリーム
+     * @param handler 処理結果を扱うためのハンドラオブジェクト
+     */
+    void postDiary(String title, String body, String group,
+            boolean showUsers, InputStream image, GetIdCallbackHandler handler);
 
 }
--- src/jp/eisbahn/android/sdk/wrapper/diary/DiaryProxyImpl.java
+++ src/jp/eisbahn/android/sdk/wrapper/diary/DiaryProxyImpl.java
@@ -1,7 +1,14 @@
 package jp.eisbahn.android.sdk.wrapper.diary;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import android.util.Log;
 
 import jp.eisbahn.android.sdk.wrapper.AbstractProxyImpl;
 import jp.eisbahn.android.sdk.wrapper.DiaryAPI;
@@ -15,6 +22,9 @@ import jp.mixi.android.sdk.MixiContainer;
  *
  */
 public class DiaryProxyImpl extends AbstractProxyImpl implements DiaryAPI {
+
+    /** バッファサイズ. */
+    private static final int BUF_SIZE = 1024;
 
     /**
      * 指定されたパラメータでこのインスタンスを初期化します.
@@ -56,6 +66,43 @@ public class DiaryProxyImpl extends AbstractProxyImpl implements DiaryAPI {
         postInternal(title, body, Visibility.group, group, showUsers, handler);
     }
 
+    @Override
+    public void postPublicDiary(final String title, final String body,
+            final InputStream image, final GetIdCallbackHandler handler) {
+        postInternal(title, body, Visibility.everyone, null, true, image,
+                handler);
+    }
+
+    @Override
+    public void postDiary(final String title, final String body,
+            final Visibility visibility, final InputStream image,
+            final GetIdCallbackHandler handler) {
+        postInternal(title, body, visibility, null, false, image, handler);
+    }
+
+    @Override
+    public void postDiary(final String title, final String body,
+            final String group, final InputStream image,
+            final GetIdCallbackHandler handler) {
+        postInternal(title, body, Visibility.group, group, false, image,
+                handler);
+    }
+
+    @Override
+    public void postDiary(final String title, final String body,
+            final Visibility visibility, final boolean showUsers,
+            final InputStream image, final GetIdCallbackHandler handler) {
+        postInternal(title, body, visibility, null, showUsers, image, handler);
+    }
+
+    @Override
+    public void postDiary(final String title, final String body,
+            final String group, final boolean showUsers,
+            final InputStream image, final GetIdCallbackHandler handler) {
+        postInternal(title, body, Visibility.group, group, showUsers, image,
+                handler);
+    }
+
     /**
      * 日記の投稿を実際に行う内部メソッドです.
      * @param title 日記のタイトル
@@ -69,24 +116,104 @@ public class DiaryProxyImpl extends AbstractProxyImpl implements DiaryAPI {
             final Visibility visibility, final String group,
             final boolean showUsers, final GetIdCallbackHandler handler) {
         try {
-            JSONObject params = new JSONObject();
-            params.put("title", title);
-            params.put("body", body);
-            JSONObject privacy = new JSONObject();
-            privacy.put("visibility", visibility.toString());
-            if (group != null) {
-                privacy.put("group", group);
-            }
-            if (showUsers) {
-                privacy.put("show_users", "1");
-            } else {
-                privacy.put("show_users", "0");
-            }
-            params.put("privacy", privacy);
+            JSONObject params = createRequestJSONObject(title, body,
+                    visibility, group, showUsers);
             getContainer().send("/diary/articles/@me/@self", params, handler);
         } catch (JSONException e) {
             throw new IllegalStateException(e);
         }
+    }
+
+    /**
+     * 日記の投稿を実際に行う内部メソッドです.
+     * @param title 日記のタイトル
+     * @param body 日記の本文
+     * @param visibility 公開範囲
+     * @param group 特定のグループに公開する際のグループID
+     * @param showUsers 「自分だけに表示」の場合はfalse,「自分と公開中の友人に表示」の場合はtrue
+     * @param image 画像の入力ストリーム
+     * @param handler 処理結果を扱うためのハンドラオブジェクト
+     */
+    private void postInternal(final String title, final String body,
+            final Visibility visibility, final String group,
+            final boolean showUsers, final InputStream image,
+            final GetIdCallbackHandler handler) {
+        try {
+            JSONObject params = createRequestJSONObject(title, body,
+                    visibility, group, showUsers);
+            StringBuilder sb = new StringBuilder();
+            sb.append("---mixi_android_sdk_wrapper\r\n");
+            sb.append("Content-Disposition: form-data; name=\"request\"\r\n");
+            sb.append("\r\n");
+            sb.append(params.toString());
+            sb.append("\r\n");
+            sb.append("---mixi_android_sdk_wrapper\r\n");
+            sb.append("Content-Disposition: form-data; name=\"photo1\";"
+                    + " filename=\"photo1.jpg\"\r\n");
+            sb.append("\r\n");
+            byte[] header = sb.toString().getBytes("UTF-8");
+            byte[] request = new byte[header.length + image.available()];
+            System.arraycopy(header, 0, request, 0, header.length);
+
+            int len = 0;
+            int offset = header.length;
+            byte[] buf = new byte[BUF_SIZE];
+            while ((len = image.read(buf)) != -1) {
+                System.arraycopy(buf, 0, request, offset, len);
+                offset += len;
+            }
+            getContainer().send(
+                    "/diary/articles/@me/@self",
+                    "multipart/form-data; boundary=-mixi_android_sdk_wrapper",
+                    new ByteArrayInputStream(request),
+                    request.length,
+                    handler);
+        } catch (JSONException e) {
+            Log.w("mixiAndroidSDKWrapper", e);
+            throw new IllegalStateException(e);
+        } catch (UnsupportedEncodingException e) {
+            Log.w("mixiAndroidSDKWrapper", e);
+            throw new IllegalStateException(e);
+        } catch (IOException e) {
+            Log.w("mixiAndroidSDKWrapper", e);
+            throw new IllegalStateException(e);
+        } finally {
+            try {
+                image.close();
+            } catch (IOException ex) {
+                Log.w("mixiAndroidSDKWrapper", ex);
+            }
+        }
+    }
+
+    /**
+     * 指定された内容に基づいて、送信されるJSONオブジェクトを構築し返します.
+     * @param title タイトル文字列
+     * @param body 本文
+     * @param visibility 公開範囲
+     * @param group グループID
+     * @param showUsers 「自分だけに表示」の場合はfalse,「自分と公開中の友人に表示」の場合はtrue
+     * @return JSONオブジェクト
+     * @throws JSONException JSONオブジェクトの構築に失敗したとき
+     */
+    private JSONObject createRequestJSONObject(final String title,
+            final String body, final Visibility visibility, final String group,
+            final boolean showUsers) throws JSONException {
+        JSONObject params = new JSONObject();
+        params.put("title", title);
+        params.put("body", body);
+        JSONObject privacy = new JSONObject();
+        privacy.put("visibility", visibility.toString());
+        if (group != null) {
+            privacy.put("group", group);
+        }
+        if (showUsers) {
+            privacy.put("show_users", "1");
+        } else {
+            privacy.put("show_users", "0");
+        }
+        params.put("privacy", privacy);
+        return params;
     }
 
 }
