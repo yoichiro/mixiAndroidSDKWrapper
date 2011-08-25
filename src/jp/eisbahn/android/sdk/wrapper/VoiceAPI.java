a85c9b2d6d881f30787618d6f3717c4eb69d6c39
yoichiro <yoichiro@eisbahn.jp>
2011-08-25 09:33:57 +0900

    Adding some methods to retrieve information for Voice API.

--- src/jp/eisbahn/android/sdk/wrapper/VoiceAPI.java
+++ src/jp/eisbahn/android/sdk/wrapper/VoiceAPI.java
@@ -1,5 +1,11 @@
 package jp.eisbahn.android.sdk.wrapper;
 
+import jp.eisbahn.android.sdk.wrapper.voice.GetCommentsCallbackHandler;
+import jp.eisbahn.android.sdk.wrapper.voice.GetFeedbacksParams;
+import jp.eisbahn.android.sdk.wrapper.voice.GetStatusParams;
+import jp.eisbahn.android.sdk.wrapper.voice.GetStatusesCallbackHandler;
+import jp.eisbahn.android.sdk.wrapper.voice.GetStatusesParams;
+import jp.eisbahn.android.sdk.wrapper.voice.GetUsersCallbackHandler;
 
 /**
  * Voice API を規定するインタフェースです.
@@ -7,5 +13,113 @@ package jp.eisbahn.android.sdk.wrapper;
  *
  */
 public interface VoiceAPI {
+
+    /**
+     * 認可ユーザのつぶやき一覧を取得します.
+     * @param handler 処理結果を扱うためのハンドラオブジェクト
+     */
+    void getMyStatuses(GetStatusesCallbackHandler handler);
+
+    /**
+     * 認可ユーザのつぶやき一覧を取得します.
+     * @param params 取得条件を指定するためのオブジェクト
+     * @param handler 処理結果を扱うためのハンドラオブジェクト
+     */
+    void getMyStatuses(GetStatusesParams params,
+            GetStatusesCallbackHandler handler);
+
+    /**
+     * 指定された特定の友人のつぶやき一覧を取得します.
+     * @param userId ユーザID
+     * @param handler 処理結果を扱うためのハンドラオブジェクト
+     */
+    void getFriendStatuses(String userId, GetStatusesCallbackHandler handler);
+
+    /**
+     * 指定された特定の友人のつぶやき一覧を取得します.
+     * @param userId ユーザID
+     * @param params 取得条件を指定するためのオブジェクト
+     * @param handler 処理結果を扱うためのハンドラオブジェクト
+     */
+    void getFriendStatuses(String userId, GetStatusesParams params,
+            GetStatusesCallbackHandler handler);
+
+    /**
+     * 全ての友人のつぶやき一覧を取得します.
+     * @param handler 処理結果を扱うためのハンドラオブジェクト
+     */
+    void getFriendsStatuses(GetStatusesCallbackHandler handler);
+
+    /**
+     * 全ての友人のつぶやき一覧を取得します.
+     * @param params 取得条件を指定するためのオブジェクト
+     * @param handler 処理結果を扱うためのハンドラオブジェクト
+     */
+    void getFriendsStatuses(GetStatusesParams params,
+            GetStatusesCallbackHandler handler);
+
+    /**
+     * 指定されたグループのつぶやき一覧を取得します.
+     * @param groupId グループID
+     * @param handler 処理結果を扱うためのハンドラオブジェクト
+     */
+    void getGroupStatuses(String groupId, GetStatusesCallbackHandler handler);
+
+    /**
+     * 指定されたグループのつぶやき一覧を取得します.
+     * @param groupId グループID
+     * @param params 取得条件を指定するためのオブジェクト
+     * @param handler 処理結果を扱うためのハンドラオブジェクト
+     */
+    void getGroupStatuses(String groupId, GetStatusesParams params,
+            GetStatusesCallbackHandler handler);
+
+    /**
+     * 指定されたIDのつぶやき一覧を取得します.
+     * @param postId つぶやきのID
+     * @param handler 処理結果を扱うためのハンドラオブジェクト
+     */
+    void getStatus(String postId, GetStatusesCallbackHandler handler);
+
+    /**
+     * 指定されたつぶやき一覧を取得します.
+     * @param postId つぶやきのID
+     * @param params 取得条件を指定するためのオブジェクト
+     * @param handler 処理結果を扱うためのハンドラオブジェクト
+     */
+    void getStatus(String postId, GetStatusParams params,
+            GetStatusesCallbackHandler handler);
+
+    /**
+     * 指定されたIDのつぶやきのコメント一覧を取得します.
+     * @param postId つぶやきのID
+     * @param handler 処理結果を扱うためのハンドラオブジェクト
+     */
+    void getStatusComments(String postId, GetCommentsCallbackHandler handler);
+
+    /**
+     * 指定されたIDのつぶやきのコメント一覧を取得します.
+     * @param postId つぶやきのID
+     * @param params 取得条件を指定するためのオブジェクト
+     * @param handler 処理結果を扱うためのハンドラオブジェクト
+     */
+    void getStatusComments(String postId, GetFeedbacksParams params,
+            GetCommentsCallbackHandler handler);
+
+    /**
+     * 指定されたIDのつぶやきのいいね一覧を取得します.
+     * @param postId つぶやきのID
+     * @param handler 処理結果を扱うためのハンドラオブジェクト
+     */
+    void getStatusFavorites(String postId, GetUsersCallbackHandler handler);
+
+    /**
+     * 指定されたIDのつぶやきのいいね一覧を取得します.
+     * @param postId つぶやきのID
+     * @param params 取得条件を指定するためのオブジェクト
+     * @param handler 処理結果を扱うためのハンドラオブジェクト
+     */
+    void getStatusFavorites(String postId, GetFeedbacksParams params,
+            GetUsersCallbackHandler handler);
 
 }
--- src/jp/eisbahn/android/sdk/wrapper/voice/VoiceProxyImpl.java
+++ src/jp/eisbahn/android/sdk/wrapper/voice/VoiceProxyImpl.java
@@ -19,4 +19,98 @@ public class VoiceProxyImpl extends AbstractProxyImpl implements VoiceAPI {
         super(newContainer);
     }
 
+    @Override
+    public void getMyStatuses(final GetStatusesCallbackHandler handler) {
+        getFriendStatuses("@me", handler);
+    }
+
+    @Override
+    public void getMyStatuses(final GetStatusesParams params,
+            final GetStatusesCallbackHandler handler) {
+        getFriendStatuses("@me", params, handler);
+    }
+
+    @Override
+    public void getFriendStatuses(final String userId,
+            final GetStatusesCallbackHandler handler) {
+        getContainer().send("/voice/statuses/" + userId + "/user_timeline",
+                handler);
+    }
+
+    @Override
+    public void getFriendStatuses(final String userId,
+            final GetStatusesParams params,
+            final GetStatusesCallbackHandler handler) {
+        getContainer().send("/voice/statuses/" + userId + "/user_timeline",
+                params.convertParameterMap(), handler);
+    }
+
+    @Override
+    public void getFriendsStatuses(final GetStatusesCallbackHandler handler) {
+        getContainer().send("/voice/statuses/friends_timeline", handler);
+    }
+
+    @Override
+    public void getFriendsStatuses(final GetStatusesParams params,
+            final GetStatusesCallbackHandler handler) {
+        getContainer().send("/voice/statuses/friends_timeline",
+                params.convertParameterMap(), handler);
+    }
+
+    @Override
+    public void getGroupStatuses(final String groupId,
+            final GetStatusesCallbackHandler handler) {
+        getContainer().send("/voice/statuses/friends_timeline/" + groupId,
+                handler);
+    }
+
+    @Override
+    public void getGroupStatuses(final String groupId,
+            final GetStatusesParams params,
+            final GetStatusesCallbackHandler handler) {
+        getContainer().send("/voice/statuses/friends_timeline/" + groupId,
+                params.convertParameterMap(), handler);
+    }
+
+    @Override
+    public void getStatus(final String postId,
+            final GetStatusesCallbackHandler handler) {
+        getContainer().send("/voice/statuses/" + postId, handler);
+    }
+
+    @Override
+    public void getStatus(final String postId, final GetStatusParams params,
+            final GetStatusesCallbackHandler handler) {
+        getContainer().send("/voice/statuses/" + postId,
+                params.convertParameterMap(), handler);
+    }
+
+    @Override
+    public void getStatusComments(final String postId,
+            final GetCommentsCallbackHandler handler) {
+        getContainer().send("/voice/replies/" + postId, handler);
+    }
+
+    @Override
+    public void getStatusComments(final String postId,
+            final GetFeedbacksParams params,
+            final GetCommentsCallbackHandler handler) {
+        getContainer().send("/voice/replies/" + postId,
+                params.convertParameterMap(), handler);
+    }
+
+    @Override
+    public void getStatusFavorites(final String postId,
+            final GetUsersCallbackHandler handler) {
+        getContainer().send("/voice/favorites/" + postId, handler);
+    }
+
+    @Override
+    public void getStatusFavorites(final String postId,
+            final GetFeedbacksParams params,
+            final GetUsersCallbackHandler handler) {
+        getContainer().send("/voice/favorites/" + postId,
+                params.convertParameterMap(), handler);
+    }
+
 }
